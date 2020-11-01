077e68a5d2efecd4ea31749373cbb38db07ab44b
koutann <koutann@163.com>
2020-11-01 22:52:12 +0800

    第三周作业提交 HttpInboundHandler.java 增加处理类

--- src/main/java/Week_02/gateway/inbound/HttpInboundHandler.java
+++ src/main/java/Week_02/gateway/inbound/HttpInboundHandler.java
@@ -1,7 +1,9 @@
 package Week_02.gateway.inbound;
 
 import Week_02.gateway.outbound.HttpOutboundHandler;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.FullHttpRequest;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,5 +20,16 @@ public class HttpInboundHandler extends ChannelInboundHandlerAdapter {
     public HttpInboundHandler(String proxyServer) {
         this.proxyServer = proxyServer;
         handler = new HttpOutboundHandler(this.proxyServer);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        FullHttpRequest fullRequest = (FullHttpRequest) msg;
+        handler.handle(fullRequest, ctx);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
     }
 }
